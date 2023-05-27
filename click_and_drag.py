d8e745fb8e1acf52462fe6dfb8ca989b017bd79f
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2023-05-28 00:42:48 +0100

    Global changes
    Got rid of global variables in the click and drag, inserted them into class constructor as parameters instead.

--- click_and_drag.py
+++ click_and_drag.py
@@ -10,16 +10,16 @@ class Drag:
         self.widget = widget
         self.widget.bind("<ButtonPress-1>", self.start_drag)
         self.widget.bind("<B1-Motion>", self.drag)
+        self.last_x = None
+        self.last_y = None
 
     def start_drag(self, event):
-        global last_x, last_y
-        last_x = event.x_root
-        last_y = event.y_root
+        self.last_x = event.x_root
+        self.last_y = event.y_root
 
     def drag(self, event):
-        global last_x, last_y
-        x = self.root.winfo_x() + (event.x_root - last_x)
-        y = self.root.winfo_y() + (event.y_root - last_y)
+        x = self.root.winfo_x() + (event.x_root - self.last_x)
+        y = self.root.winfo_y() + (event.y_root - self.last_y)
         self.root.geometry(f"+{x}+{y}")
-        last_x = event.x_root
-        last_y = event.y_root
+        self.last_x = event.x_root
+        self.last_y = event.y_root
